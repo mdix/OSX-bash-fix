6b88cc40c69f2b3a4477ad7dd73d6cdca46e3702
Marc Dix <marcdix@marcdix.de>
2014-09-25 13:19:18 +0200

    fixed path to builddir

--- replace.sh
+++ replace.sh
@@ -4,8 +4,8 @@ sudo cp /bin/bash /bin/bash.old
 sudo cp /bin/sh /bin/sh.old
 
 echo Copying new bash and sh binaries
-sudo cp build/Release/bash /bin
-sudo cp build/Release/sh /bin
+sudo cp bash-fix/bash-92/build/Release/bash /bin
+sudo cp bash-fix/bash-92/build/Release/sh /bin
 
 echo Removing executable bit from old bash and sh binary
 sudo chmod a-x /bin/bash.old /bin/sh.old
