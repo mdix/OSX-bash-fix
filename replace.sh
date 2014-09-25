f57b63343955efb924132eeb51ada3f50ee79745
Marc Dix <marcdix@marcdix.de>
2014-09-25 09:57:05 +0200

    Some fixes

--- replace.sh
+++ replace.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
+echo Renaming old bash and sh binaries
 sudo cp /bin/bash /bin/bash.old
 sudo cp /bin/sh /bin/sh.old
-build/Release/bash --version # GNU bash, version 3.2.52(1)-release
-build/Release/sh --version   # GNU bash, version 3.2.52(1)-release
+
+echo Copying new bash and sh binaries
 sudo cp build/Release/bash /bin
 sudo cp build/Release/sh /bin
+
+echo Removing executable bit from old bash and sh binary
 sudo chmod a-x /bin/bash.old /bin/sh.old
