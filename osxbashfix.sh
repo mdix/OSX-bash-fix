6a167aae781fd25236a5c09a0ee27dcbdbc0c25e
Marc Dix <marcdix@marcdix.de>
2014-09-25 09:27:58 +0200

    Added chmod as suggested

--- osxbashfix.sh
+++ osxbashfix.sh
@@ -13,3 +13,4 @@ build/Release/bash --version # GNU bash, version 3.2.52(1)-release
 build/Release/sh --version   # GNU bash, version 3.2.52(1)-release
 sudo cp build/Release/bash /bin
 sudo cp build/Release/sh /bin
+sudo chmod a-x /bin/bash.old /bin/sh.old
